a84caa3a9a7861d124587a09a861e93d2dc3af15
Amy Mok <amy.mok@cfpb.gov>
2018-02-13 02:01:57 -0500

    Add tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build Status](https://travis-ci.org/amymok/miro.svg?branch=master)](https://travis-ci.org/amymok/miro)[![Coverage Status](https://coveralls.io/repos/github/amymok/miro/badge.svg?branch=master)](https://coveralls.io/github/amymok/miro?branch=master)
+
 # miro
--- id_generator/tests.py
+++ id_generator/tests.py
@@ -1,3 +1,83 @@
-from django.test import TestCase
+import string
+from django.urls import reverse
+from django.core.cache import cache
+from rest_framework import status
+from rest_framework.test import APITestCase
+import unittest.mock as mock
+from id_generator.throttling import BurstRateThrottle, SustainedRateThrottle
 
-# Create your tests here.
+
+class IdGeneratorTests(APITestCase):
+
+    def setUp(self):
+        self.orig_burst_rate = BurstRateThrottle.rate
+        self.orig_sustained_rate = SustainedRateThrottle.rate
+        # Setting rates to something really big so it doesn't affect testing
+        BurstRateThrottle.rate = '2000/min'
+        SustainedRateThrottle.rate = '2000/day'
+
+    def tearDown(self):
+        cache.clear()
+        BurstRateThrottle.rate = self.orig_burst_rate
+        SustainedRateThrottle.rate = self.orig_sustained_rate
+
+    def test_generate_miro_subject_id__valid(self):
+        """
+        Ensure we can generate a Miro ID
+        """
+        url = reverse('id_generator:generate')
+        data = {
+            'study_id': 'interesting-study.prestigious.edu',
+            'study_subject_id': '�👍'
+        }
+
+        response = self.client.get(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(type(response.data), dict)
+        self.assertEqual(len(response.data.keys()), 1)
+        self.assertIn("miro_subject_id", response.data)
+        self.assertTrue(all(c in string.hexdigits
+            for c in response.data["miro_subject_id"]))
+
+    def test_generate_miro_subject_id_repeat__valid(self):
+        """
+        Ensure we can generate a Miro ID repeatedly
+        """
+        url = reverse('id_generator:generate')
+        data = {
+            'study_id': 'interesting-study.prestigious.edu',
+            'study_subject_id': '�👍'
+        }
+
+        response = self.client.get(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(type(response.data), dict)
+        self.assertEqual(len(response.data.keys()), 1)
+        self.assertIn("miro_subject_id", response.data)
+        self.assertTrue(all(c in string.hexdigits
+            for c in response.data["miro_subject_id"]))
+
+        exp_id = response.data["miro_subject_id"]
+        for i in range(10):
+            response = self.client.get(url, data)
+            self.assertEqual(response.data.get("miro_subject_id"), exp_id)
+
+    def test_generate_miro_subject_id__invalid(self):
+        """
+        Ensure we will receive errors and 400 if invalid parameters provided
+        """
+        url = reverse('id_generator:generate')
+        data = {
+            'study_id': ' ',
+            'study_subject_id': ' '
+        }
+
+        response = self.client.get(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data.keys()), 2)
+        self.assertIn("study_id", response.data)
+        self.assertEqual(["This field may not be blank."],
+            response.data["study_id"])
+        self.assertIn("study_subject_id", response.data)
+        self.assertEqual(["This field may not be blank."],
+            response.data["study_subject_id"])
